9df0f6b0d5fe2454c32e39929ba395e4dae0a079
Alex <kuzminbass@gmail.com>
2021-12-17 07:53:26 +1000

    more tests for datareader

--- utils/dataReader.test.js
+++ utils/dataReader.test.js
@@ -8,3 +8,13 @@ test("Check that the input file can be read into array", async () => {
   const data = await dr.readFile();
   expect(Array.isArray(data)).toBe(true);
 });
+
+test("Check that array contains a specific name", async () => {
+  const dr = new DataReader(inputFile);
+  await expect(dr.readFile()).resolves.toContain("Marin Alvarez");
+});
+
+test('Check that non-existing file throws error', async () => {
+  const dr = new DataReader('abc');
+  await expect(dr.readFile).rejects.toMatch('Error');
+});
