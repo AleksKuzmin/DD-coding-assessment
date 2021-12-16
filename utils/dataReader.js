a55c938c76ab817f661c1516edc0802ce1dc360f
Alex <kuzminbass@gmail.com>
2021-12-16 12:34:48 +1000

    change

---
+++ utils/dataReader.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+
+export default class DataReader {
+  constructor(filename) {
+    this.filename = filename;
+  }
+
+  readFile() {
+    return new Promise((resolve, reject) => {
+      fs.readFile(this.filename, "utf-8", function (error, data) {
+        if (error) {
+          console.log(error);
+          reject(error);
+        }
+        resolve(data.split("\n"));
+      });
+    });
+  }
+}
