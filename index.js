ca542b72ee463675978280dacad9969c05086956
Alex <kuzminbass@gmail.com>
2021-12-16 13:46:12 +1000

    writer

--- index.js
+++ index.js
@@ -1,14 +1,22 @@
 import DataReader from "./utils/dataReader.js";
 import DataSorter from "./utils/dataSorter.js";
+import DataWriter from "./utils/dataWriter.js";
 
 const inputFileName = "unsorted-names-list.txt";
 const outputFileName = "sorted-names-list.txt";
 
 const dataReader = new DataReader(inputFileName);
-
+const dataWriter = new DataWriter(outputFileName);
 const dataSorter = new DataSorter();
 
 dataReader.readFile().then((unsortedData) => {
   const sortedData = dataSorter.sortRecords(unsortedData);
-  console.log(sortedData);
+  dataWriter
+    .writeFile(sortedData)
+    .then(() => {
+      console.log(sortedData);
+    })
+    .catch((r) => {
+      console.log("error occured while writing to file " + r);
+    });
 });
